c72abe87fdd92de11acb0a4b5b3019c0fc70dc54
allan.pirillis <allan.p94@gmail.com>
2021-05-10 18:05:56 -0500

    set up routes for each of the 3 js file in the public folder

--- server.js
+++ server.js
@@ -128,6 +128,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Api server now on port ${PORT}!`);
 });
